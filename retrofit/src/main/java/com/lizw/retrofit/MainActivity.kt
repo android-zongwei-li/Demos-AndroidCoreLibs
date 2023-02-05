387a86b10d027d456c90ecb0f9c8f6e1f3fbc647
lizw <893846649@qq.com>
2023-02-05 23:23:27 +0800

    <retrofit> + 基本使用-域名访问

--- retrofit/src/main/java/com/lizw/retrofit/MainActivity.kt
+++ retrofit/src/main/java/com/lizw/retrofit/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.lizw.retrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.lizw.retrofit.basicuse.BaiduService
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.baidu.com/")
+            .build()
+
+        val service = retrofit.create(BaiduService::class.java)
+        val repos: Call<ResponseBody> = service.listRepos()
+        repos.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                Log.i(TAG, response.toString())
+                Log.i(TAG, response.body()!!.source().toString())
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Log.i(TAG, t.toString())
+            }
+        })
+
     }
 }
---
+++ retrofit/src/main/java/com/lizw/retrofit/docs/retrofit相关问题.md
@@ -0,0 +1,13 @@
+一、原理
+Retrofit的作用
+Retrofit主要实现机制？
+网络封装框架：Retrofit实现原理
+Retrofit的流程
+Retrofit在OkHttp上做了哪些封装？
+retrofit源码分析
+
+二、注解、动态代理
+动态代理和静态代理的区别，是怎么实现的
+Retrofit的注解是怎么解析的：动态代理、AOP
+Retrofit中的动态代理
+Retrofit原理？为何用代理？代理的作用是什么？
