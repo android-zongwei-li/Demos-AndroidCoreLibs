e0dea10f57d1d2f8e49751d357c8ff45490e7ccb
zongwei.li <893846649@qq.com>
2023-05-15 15:21:34 +0800

    <project> - 移除glide、okhttp、retrofit、rxjava模块，分别改为core_apis中的一个package

--- core_apis/src/main/java/com/lizw/core_apis/MainActivity.kt
+++ core_apis/src/main/java/com/lizw/core_apis/MainActivity.kt
@@ -11,21 +11,22 @@ import androidx.core.graphics.drawable.toBitmap
 import com.lizw.core_apis.android.LruCacheManager
 import com.lizw.core_apis.databinding.ActivityMainBinding
 import com.lizw.core_apis.kotlin.coroutines.CoroutinesActivity
+import com.lizw.core_apis.thirdpartlibs.retrofit.RetrofitActivity
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        
         binding.btnStartActivityCoroutines.setOnClickListener {
             startActivity(Intent(this, CoroutinesActivity::class.java))
         }
-
+        
         binding.btnLoadBitmap.setOnClickListener {
             val startTime = System.currentTimeMillis()
             val bitmap = ImageLoader.loadDefaultBitmap(this)
@@ -33,32 +34,36 @@ class MainActivity : AppCompatActivity() {
             val spentTime = System.currentTimeMillis() - startTime
             Log.i(TAG, "spentTime = $spentTime ms")
         }
+        
+        binding.btnStartRetrofitActivity.setOnClickListener {
+            startActivity(Intent(this, RetrofitActivity::class.java))
+        }
     }
-
+    
     private class ImageLoader {
         companion object {
             private const val TAG = "ImageLoader"
-
+            
             var lruCacheManager: LruCacheManager? = null
-
+            
             fun loadDefaultBitmap(context: Context): Bitmap? {
                 var result: Bitmap?
                 if (lruCacheManager == null) {
                     lruCacheManager = LruCacheManager(context)
                 }
-
+                
                 result = lruCacheManager?.getBitmap("defaultBitmap")
                 Log.i(TAG, "loaded from LruCache, result = $result")
-
+                
                 if (result == null) {
                     // 下面这行加载不到bitmap图片，why？
 //                    result = BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher)
                     result =
                         ResourcesCompat.getDrawable(context.resources, R.mipmap.ic_launcher, null)
-                            ?.toBitmap()
+                                ?.toBitmap()
                     Log.i(TAG, "load from file, result = $result")
                 }
-
+                
                 return result.apply {
                     lruCacheManager?.putCache("defaultBitmap", result)
                 }
