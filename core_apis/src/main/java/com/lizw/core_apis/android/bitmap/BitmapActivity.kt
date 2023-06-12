f12637c022986c0c5b38fe38bc4cb6dfc7f67255
zongwei.li <893846649@qq.com>
2023-06-12 13:01:56 +0800

    [project] + demo 大图加载

--- core_apis/src/main/java/com/lizw/core_apis/android/bitmap/BitmapActivity.kt
+++ core_apis/src/main/java/com/lizw/core_apis/android/bitmap/BitmapActivity.kt
@@ -1,35 +1,61 @@
 package com.lizw.core_apis.android.bitmap
 
-import android.os.Bundle
-import com.lizw.core_apis.R
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import com.lizw.core_apis.android.bitmap.ImageUtils
-import android.os.Build
-import android.widget.ImageView
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.lizw.core_apis.R
+import com.lizw.core_apis.databinding.ActivityBitmapBinding
 
+// todo 写一个面板显示当前图片的信息。其他页面也类似，把需要从logcat查看的数据显示到应用中
 class BitmapActivity : AppCompatActivity() {
-    var imageView: ImageView? = null
+    companion object {
+        private const val TAG = "BitmapActivity"
+    }
+    
+    private lateinit var binding: ActivityBitmapBinding
+    
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bitmap)
-        imageView = findViewById(R.id.iv_big_view)
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
+        binding = ActivityBitmapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        resources.displayMetrics.apply {
+            Log.i(TAG, "density = " + this.density.toString())
+            Log.i(TAG, "densityDpi = " + this.densityDpi.toString())
+        }
+        
         // 初始图片
         val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.big_image)
-        ImageUtils.printBitmapInfo(bitmap)
-
-        // 压缩后
-        val bitmap1 = ImageUtils.resizeBitmap(resources,
-            R.mipmap.big_image,
-            imageView!!.width,
-            imageView!!.height,
-            false)
-        ImageUtils.printBitmapInfo(bitmap1)
-        imageView!!.setImageBitmap(bitmap1)
+        binding.tvBitmapInfo.text = ImageUtils.getBitmapInfo(bitmap).apply {
+            Log.i(TAG, this)
+        }
+        // binding.ivBigView.setImageBitmap(bitmap)
+        
+        binding.btnResize.setOnClickListener {
+            // 压缩后
+            val bitmap1 = ImageUtils.resizeBitmap(resources, R.mipmap.big_image1,
+                    binding.ivBigView.width, binding.ivBigView.height, false)
+            binding.tvBitmapInfo.text = "${binding.tvBitmapInfo.text} \n${ImageUtils.getBitmapInfo(bitmap1)}"
+            binding.ivBigView.setImageBitmap(bitmap1)
+        }
+        
+        val handler = object : Handler(Looper.getMainLooper()) {
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+                val bitmap2 = msg.obj as Bitmap
+                binding.tvBitmapInfo.text = ImageUtils.getBitmapInfo(bitmap2)
+                binding.ivBigView.setImageBitmap(bitmap2)
+            }
+        }
+        binding.btnLoadNetBitmap.setOnClickListener {
+            ImageUtils.loadBitmapFromNet(handler)
+        }
     }
 }
--- core_apis/src/main/java/com/lizw/core_apis/android/bitmap/ImageUtils.kt
+++ core_apis/src/main/java/com/lizw/core_apis/android/bitmap/ImageUtils.kt
@@ -3,29 +3,81 @@ package com.lizw.core_apis.android.bitmap
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.os.Build
+import android.os.Handler
+import android.os.Message
 import android.util.Log
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 object ImageUtils {
     fun resizeBitmap(
-        resources: Resources, id: Int,
-        maxW: Int, maxH: Int, hasAlpha: Boolean,
+            resources: Resources, id: Int,
+            maxW: Int, maxH: Int, hasAlpha: Boolean,
     ): Bitmap {
         val options = BitmapFactory.Options()
-        // 只解码出相关的参数
+        // 设置为true后，只解码出图片的参数，比如图片的格式、尺寸等，不会加载具体的图片
         options.inJustDecodeBounds = true
+        // 解码后，会将图片的参数保存到 options 中，后续可通过 options 获取图片参数
         BitmapFactory.decodeResource(resources, id, options)
+        
+        // 图片类型
+        val imageType = options.outMimeType
+        
+        // 获取图片的宽度和高度
         val originalWidth = options.outWidth
         val originalHeight = options.outHeight
-        // 设置缩放系数，只能取2的n次方
+        // 设置缩放系数（采样率），只能取2的n次方。比如采样率为2时，加载的图片宽高均为原始图片的 1/2，总的大小就是图片的 1/4，以此类推。
         options.inSampleSize = calculateInSampleSize(originalWidth, originalHeight, maxW, maxH)
+        
         if (!hasAlpha) {
             options.inPreferredConfig = Bitmap.Config.RGB_565
         }
+        
+        // 设置为false后，就会加载 Bitmap 到内存中
         options.inJustDecodeBounds = false
         return BitmapFactory.decodeResource(resources, id, options)
     }
-
+    
+    fun loadBitmapFromNet(handler: Handler) {
+        val t: Thread = object : Thread() {
+            override fun run() {
+                super.run()
+                try {
+                    val url = URL("https://img2.baidu.com/it/u=3442533269,4290406331&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500")
+                    var connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                    println("    code :" + connection.responseCode)
+                    val options = BitmapFactory.Options()
+                    options.inJustDecodeBounds = true
+                    val `is`: InputStream = connection.inputStream
+                    BitmapFactory.decodeStream(`is`, null, options)
+                    
+                    val height = options.outHeight
+                    val width = options.outWidth
+                    // 获取图片的类型
+                    val imageType = options.outMimeType
+                    Log.i("imageTest", "$width    $height   $imageType")
+                    
+                    connection = url.openConnection() as HttpURLConnection
+                    
+                    options.inJustDecodeBounds = false
+                    options.inSampleSize = 2
+                    val bitmap = BitmapFactory.decodeStream(connection.inputStream, null, options)
+                    val m = Message()
+                    m.obj = bitmap
+                    handler.sendMessage(m)
+                } catch (e: MalformedURLException) {
+                    e.printStackTrace()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        t.start()
+    }
+    
     /**
      * 计算 inSampleSize：缩放系数，只能取2的n次方
      *
@@ -35,8 +87,8 @@ object ImageUtils {
      * @param maxH           目标高度
      */
     private fun calculateInSampleSize(
-        originalWidth: Int, originalHeight: Int,
-        maxW: Int, maxH: Int,
+            originalWidth: Int, originalHeight: Int,
+            maxW: Int, maxH: Int,
     ): Int {
         var inSampleSize = 1
         if (originalWidth > maxW && originalHeight > maxH) {
@@ -47,11 +99,9 @@ object ImageUtils {
         }
         return inSampleSize
     }
-
-    fun printBitmapInfo(bitmap: Bitmap?) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Log.e("bitmap", "x : " + bitmap!!.width + " y : " + bitmap.height +
-                    " 内存大小 ： " + bitmap.allocationByteCount)
-        }
+    
+    fun getBitmapInfo(bitmap: Bitmap): String {
+        val size = bitmap.allocationByteCount
+        return "BitmapInfo: w = ${bitmap.width} h = ${bitmap.height} , 内存大小 = ${size}B(${size / 1000000.0}M)"
     }
 }
