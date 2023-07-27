2196f34187d22b8fe1ebfbe754ebfbc2b315f2f2
lizw <893846649@qq.com>
2023-07-28 01:08:36 +0800

    [UI] + Drawable渐变 + 圆形图像

--- ui_demos/src/main/java/com/lizw/ui_demos/DrawableActivity.kt
+++ ui_demos/src/main/java/com/lizw/ui_demos/DrawableActivity.kt
@@ -1,14 +1,19 @@
 package com.lizw.ui_demos
 
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.PaintDrawable
 import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.graphics.drawable.shapes.ArcShape
 import android.graphics.drawable.shapes.OvalShape
 import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.lizw.ui_demos.databinding.ActivityDrawableBinding
 
 class DrawableActivity : AppCompatActivity() {
@@ -36,5 +41,33 @@ class DrawableActivity : AppCompatActivity() {
         arcDrawable.paint.color = Color.BLUE
         arcDrawable.paint.style = Paint.Style.FILL
         viewBinding.tvDrawableArc.setBackgroundDrawable(arcDrawable)
+
+        val drawableArr = arrayOf(ColorDrawable(Color.TRANSPARENT), ColorDrawable(Color.RED))
+        val transitionDrawable = TransitionDrawable(drawableArr)
+        // 交叉淡入样式
+        transitionDrawable.isCrossFadeEnabled = true
+        transitionDrawable.startTransition(5000)
+        viewBinding.ivTransitionDrawable.setImageDrawable(transitionDrawable)
+
+        fun roundPicExample() {
+            // 原始图
+            viewBinding.ivRoundPic1.setImageResource(R.mipmap.p10)
+
+            // 圆角图
+            val roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(resources,
+                BitmapFactory.decodeResource(resources, R.mipmap.p10))
+            roundedBitmapDrawable.paint.isAntiAlias = true
+            // cornerRadius 设置为 180 也可以变成圆角
+            roundedBitmapDrawable.cornerRadius = 30f
+            viewBinding.ivRoundPic2.setImageDrawable(roundedBitmapDrawable)
+
+            // 圆形图像
+            val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.p10)
+            val roundedBitmapDrawable2 = RoundedBitmapDrawableFactory.create(resources, bitmap)
+            roundedBitmapDrawable2.paint.isAntiAlias = true
+            roundedBitmapDrawable2.cornerRadius = Math.max(bitmap.width, bitmap.height).toFloat()
+            viewBinding.ivRoundPic3.setImageDrawable(roundedBitmapDrawable2)
+        }
+        roundPicExample()
     }
 }
