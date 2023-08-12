dc8ee06c507fefefadb1a168482486b7f9781585
lizw <893846649@qq.com>
2023-08-12 23:30:47 +0800

    [core_apis] + demo 消息通知 + 增加一个通用接口，用于提供各个类涉及的技术点

--- core_apis/src/main/java/com/lizw/core_apis/CoreApisMainActivity.kt
+++ core_apis/src/main/java/com/lizw/core_apis/CoreApisMainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.lizw.common.ext.startActivityDefaultIntent
 import com.lizw.core_apis.android.activity.LifecycleTestActivity
 import com.lizw.core_apis.android.contentprovider.ContentProviderActivity
+import com.lizw.core_apis.android.notification.NotificationActivity
 import com.lizw.core_apis.databinding.ActivityCoreApisMainBinding
 import com.lizw.core_apis.java.thread.ThreadDemoActivity
 import com.lizw.core_apis.kotlin.coroutines.CoroutinesActivity
@@ -14,30 +15,34 @@ class CoreApisMainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityCoreApisMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         binding.btnStartActivityCoroutines.setOnClickListener {
             startActivityDefaultIntent(CoroutinesActivity::class.java)
         }
-        
+
         binding.btnStartRetrofitActivity.setOnClickListener {
             startActivityDefaultIntent(RetrofitActivity::class.java)
         }
-        
+
         binding.btnStartActivityThread.setOnClickListener {
             startActivityDefaultIntent(ThreadDemoActivity::class.java)
         }
-        
+
         binding.btnStartContentProvider.setOnClickListener {
             startActivityDefaultIntent(ContentProviderActivity::class.java)
         }
-        
+
         binding.btnStartActivityLifecycle.setOnClickListener {
             startActivityDefaultIntent(LifecycleTestActivity::class.java)
+        }
+
+        binding.btnStartNotification.setOnClickListener {
+            startActivityDefaultIntent(NotificationActivity::class.java)
         }
     }
 }
