33361ada3ed6da75dfb9138f46110731d424483e
lizongwei <893846649@qq.com>
2024-07-02 17:36:32 +0800

    * Glide自定义模块使用

--- core_apis/src/main/java/com/lizw/core_apis/thirdpartlibs/glide/GlideOkHttpModule.kt
+++ core_apis/src/main/java/com/lizw/core_apis/thirdpartlibs/glide/GlideOkHttpModule.kt
@@ -3,6 +3,7 @@ package com.lizw.core_apis.thirdpartlibs.glide
 import android.content.Context
 import android.util.Log
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
@@ -36,5 +37,13 @@ class GlideOkHttpModule : AppGlideModule() {
         registry.replace(
             GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(client)
         )
+    }
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+    }
+
+    override fun isManifestParsingEnabled(): Boolean {
+        return false
     }
 }
